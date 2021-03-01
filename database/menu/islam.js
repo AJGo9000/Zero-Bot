12f248956ce6e4e4e15a38f9636ca6193ab71d73
Zero <ajgo2323@gmail.com>
2021-03-01 17:19:59 -0300

    update islam.js

--- database/menu/islam.js
+++ database/menu/islam.js
@@ -2,7 +2,7 @@ const islam = (prefix, botName, ownerName) => {
         return `
 ┏ *〈 ${botName} 〉*
 ╿
-┷┯ *〈 BOT INFO 〉*
+┷┯ *〈 INFORMAÇÕES DO BOT 〉*
    ╽
    ┠≽ *Prefix* : 「  ${prefix}  」
    ┠≽ *Creator* : ${ownerName}
@@ -11,27 +11,27 @@ const islam = (prefix, botName, ownerName) => {
 ┯┷ *〈 ABOUT 〉*
 ╽
 ┠≽ *${prefix}info* (error)
-┃ *Desc* : Show Bot Details
+┃ *Desc* : Mostrar detalhes do bot
 ┠──────────────╼
 ┠≽ *${prefix}blocklist*
-┃ *Desc* : Show Blocked User
+┃ *Desc* : Mostrar usuário bloqueado
 ┠──────────────╼
 ┠≽ *${prefix}chatlist* (error)
-┃ *Desc* : Show All Chat User
+┃ *Desc* : Mostrar todos os usuários de bate-papo
 ┠──────────────╼
 ┠≽ *${prefix}ping*
-┃ *Desc* : Show Connection Bot Speed
+┃ *Desc* : Mostrar velocidade do bot de conexão
 ┠──────────────╼
-┠≽ *${prefix}bugreport* <text>
-┃ *Desc* : Report Bug To Owner Bot
+┠≽ *${prefix}bugreport* <texto>
+┃ *Desc* : Reportar bug ao proprietário do bot
 ╿
 ┷┯ *〈 ISLAM 〉*
    ╽
    ┠≽ *${prefix}quran*
-   ┃ *Desc* : Sending Qur'anic Verses at Random
+   ┃ *Desc* : Envio de versos do Alcorão ao acaso
    ┠──────────────╼
    ┠≽ *${prefix}jsholat* <area>
-   ┃ *Desc* : Sending Prayer Schedule Information
+   ┃ *Desc* : Envio de informações do cronograma de oração
    ╿ *${ownerName}*,
    ╰╼≽ *Developer © ${botName}`
 }
