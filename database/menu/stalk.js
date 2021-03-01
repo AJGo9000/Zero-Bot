7a0bd4399158eaca96ce497c09fd857a0ff074ed
Zero <ajgo2323@gmail.com>
2021-02-28 23:26:57 -0300

    update stalk.js

--- database/menu/stalk.js
+++ database/menu/stalk.js
@@ -22,7 +22,7 @@ const stalk = (prefix, botName, ownerName) => {
 ┠≽ *${prefix}ping*
 ┃ *Desc* : Mostrar velocidade do bot de conexão
 ┠──────────────╼
-┠≽ *${prefix}bugreport* <text>
+┠≽ *${prefix}bugreport* <texto>
 ┃ *Desc* : Reportar bug ao proprietário do bot
 ╿
 ┷┯ *〈 STALK 〉*
