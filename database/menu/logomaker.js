b90f9afa22343875114fa39507a2648364ade94a
Zero <ajgo2323@gmail.com>
2021-03-01 09:46:20 -0300

    update logomaker.js

--- database/menu/logomaker.js
+++ database/menu/logomaker.js
@@ -2,7 +2,7 @@ const logomaker = (prefix, botName, ownerName) => {
         return `
 ┏ *〈 ${botName} 〉*
 ╿
-┷┯ *〈 BOT INFO 〉*
+┷┯ *〈 INFORMAÇÕES DO BOT 〉*
    ╽
    ┠≽ *Prefix* : 「  ${prefix}  」
    ┠≽ *Creator* : ${ownerName}
@@ -11,47 +11,47 @@ const logomaker = (prefix, botName, ownerName) => {
 ┯┷ *〈 ABOUT 〉*
 ╽
 ┠≽ *${prefix}info* (error)
-┃ *Desc* : Show Bot Details
+┃ *Desc* : Mostrar detalhes do bot
 ┠──────────────╼
 ┠≽ *${prefix}blocklist*
-┃ *Desc* : Show Blocked User
+┃ *Desc* : Mostrar usuário bloqueado
 ┠──────────────╼
 ┠≽ *${prefix}chatlist* (error)
-┃ *Desc* : Show All Chat User
+┃ *Desc* : Mostrar todos os usuários de bate-papo
 ┠──────────────╼
 ┠≽ *${prefix}ping*
-┃ *Desc* : Show Connection Bot Speed
+┃ *Desc* : Mostrar velocidade do bot de conexão
 ┠──────────────╼
-┠≽ *${prefix}bugreport* <text>
-┃ *Desc* : Report Bug To Owner Bot
+┠≽ *${prefix}bugreport* <texto>
+┃ *Desc* : Reportar bug ao proprietário do bot
 ╿
 ┷┯ *〈 LOGO MAKER 〉*
    ╽
-   ┠≽ *${prefix}ninjalogo* <text|text>
-   ┃ *Desc* : Make Ninja Logo
+   ┠≽ *${prefix}ninjalogo* <texto|texto>
+   ┃ *Desc* : Logotipo da Make Ninja
    ┠──────────────╼
-   ┠≽ *${prefix}logowolf* <text|text>
+   ┠≽ *${prefix}logowolf* <texto|texto>
    ┃ *Desc* : Make Wolf Logo
    ┠──────────────╼
-   ┠≽ *${prefix}logowolf2* <text|text>
+   ┠≽ *${prefix}logowolf2* <texto|texto>
    ┃ *Desc* : Make Wolf Logo2
    ┠──────────────╼
-   ┠≽ *${prefix}phlogo* <text|text>
+   ┠≽ *${prefix}phlogo* <texto|texto>
    ┃ *Desc* : Make PornHub Logo
    ┠──────────────╼
-   ┠≽ *${prefix}neonlogo* <text>
+   ┠≽ *${prefix}neonlogo* <texto>
    ┃ *Desc* : Make Neon Logo
    ┠──────────────╼
-   ┠≽ *${prefix}neonlogo2* <text>
+   ┠≽ *${prefix}neonlogo2* <texto>
    ┃ *Desc* : Make Neon Logo2
    ┠──────────────╼
-   ┠≽ *${prefix}lionlogo* <text|text>
+   ┠≽ *${prefix}lionlogo* <texto|texto>
    ┃ *Desc* : Make Lion Logo
    ┠──────────────╼
-   ┠≽ *${prefix}jokerlogo* <text>
+   ┠≽ *${prefix}jokerlogo* <texto>
    ┃ *Desc* : Make Joker Logo
    ┠──────────────╼
-   ┠≽ *${prefix}pubglogo* <text|text>
+   ┠≽ *${prefix}pubglogo* <texto|texto>
    ┃ *Desc* : Make PUBG Logo
    ╿ *${ownerName}*,
    ╰╼≽ *Developer © ${botName}`
